323a4d764746050280b3432e657284b3cc2aef2b
wrigglyt <ryan@monocomputing.com>
2024-07-27 21:01:05 -0700

    check scripts

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const { exec } = require('child_process');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	exec('ls -lh \| awk \'{print $9}\'', function(err,stdout,stderr){
+	exec('ls -lh scripts \| awk \'{print $9}\'', function(err,stdout,stderr){
 		data = stdout.trim(' ').split('\n');
 		console.log(data);
 		res.render('index', { dirs: data });
